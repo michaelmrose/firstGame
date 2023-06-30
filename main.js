430de759ad6c877490189e7777d200f05b5c37a0
Michael Rose <michael@rosenetwork.net>
2023-06-30 09:20:15 -0700

    initial revision of main.js with a game class

--- main.js
+++ main.js
@@ -16,16 +16,6 @@ const southEast = { x: 1, y: -1 };
 // STATE VARIABLES
 //-------------------------------------------------------------//
 
-let board;
-let winner;
-let currentPlayer;
-let players;
-let latestInsertion;
-let message;
-let numberOfColumns;
-let numberOfRows;
-let matchesToWin;
-
 //-------------------------------------------------------------//
 // ELEMENTS
 //-------------------------------------------------------------//
@@ -33,111 +23,256 @@ let messageElement = document.getElementById("message");
 let markersElement = document.getElementById("markers");
 let boardEl = document.getElementById("board");
 
-document.getElementById("reset_button").addEventListener("click", init);
+// document.getElementById("reset_button").addEventListener("click", init);
 
 //-------------------------------------------------------------//
 // FUNCTIONS
 //-------------------------------------------------------------//
 
-//------------------------------------------------------------//
-// The core logic is contained herein.  Attempt to insert.
-// If insertion succeeds test board along possible axis for 3 addition matching
-// positions and either end the game or switch the active player.
-//------------------------------------------------------------//
-
-function handleMarkerClick(evt) {
-    //-------------------------------------------------------------//
-    // Only if clicking on a div and insertion succeeds
-    // meaning column wasn't full insert a value
-    //-------------------------------------------------------------//
-    if (evt.target.tagName !== "DIV") return; //click better
-    let latestInsertion = insert(evt.target.column, currentPlayer.boardValue);
-    if (latestInsertion) {
-        if (isWinningPosition(latestInsertion)) endGame();
-        else switchPlayer();
-        render();
+//-------------------------------------------------------------//
+// CLASSES
+//-------------------------------------------------------------//
+class Position {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    add(pos) {
+        return new Position(pos.x + this.x, pos.y + this.y);
+    }
+    set(pos) {
+        this.x = pos.x;
+        this.y = pos.y;
+    }
+    move(pos) {
+        this.x = pos.x + this.x;
+        this.y = pos.y + this.y;
+    }
+    copy() {
+        return new Position(this.x, this.y);
+    }
+    within(area) {
+        return (
+            this.x >= 0 &&
+            this.y >= 0 &&
+            this.x < area.width &&
+            this.y < area.height
+        );
     }
 }
+class Game {
+    constructor(
+        boardElement,
+        markersElement,
+        messageElement,
+        title,
+        width,
+        height,
+        matchesToWin,
+        colors
+        // sounds,
+    ) {
+        this.boardElement = boardElement;
+        this.markersElement = markersElement;
+        this.messageElement = messageElement;
+        this.title = title;
+        this.message = "TEST MSG";
+        this.colors = colors; //not meaningfully wired to anything
+        this.players = [
+            { name: "player-one", color: "purple", boardValue: 1 },
+            { name: "player-two", color: "gold", boardValue: 2 },
+        ];
+        // this.sounds = sounds; //not wired up to anything
 
-boardEl.addEventListener("click", (evt) => {
-    if (evt.target.tagName !== "DIV") return;
-    console.log(evt.target.column);
-});
-
-//-------------------------------------------------------------//
-// Inserts number representing player into first unoccupied position
-// in col, represented by current value being zero, and then returns
-// the pos of most recent insertion both to see if insertion AND
-// to test the value to see if player won
-//-------------------------------------------------------------//
-function insert(col, n) {
-    let top = board[col].indexOf(0); //first zero is the first empty slot
-
-    //if not already full
-    if (top !== -1) {
-        board[col][top] = n;
-        {
-            let latestInsertion = { x: parseInt(col), y: top };
-            return latestInsertion;
+        this.width = width;
+        this.height = height;
+        this.matchesToWin = matchesToWin;
+        this.board = [];
+        let column = repeat(0, this.height);
+        for (let i = 0; i < this.width; i++) {
+            this.board.push(column.slice(0));
         }
-    } else return false;
-}
-
-//-------------------------------------------------------------//
-// Takes position {x: n y: n} and direction {x: n y: n} and uses to them
-// to recursively find the number of positions matching the value
-// found at pos. It returns the current number of matches when we
-// either run off the board, find a non-matching value, or get to 3
-// indicating we have a total of 4 matching values in a line.
-//-------------------------------------------------------------//
-function check(pos, direction, desired = board[pos.x][pos.y], matches = 0) {
-    let dest = addCords(direction, pos);
-    if (!positionInBounds(dest)) return matches;
-    let value = board[dest.x][dest.y];
-    if (desired !== value) {
-        return matches;
+        this.buildBoard(this.height, this.width, this.boardElement);
+        this.buildMarkers(this.width, this.markersElement);
+        this.setBoardColumnsStyle(
+            this.width,
+            this.boardElement,
+            this.markersElement
+        );
+        this.currentPlayer = this.players[0];
+        this.winner = null;
+        this.message = `${this.currentPlayer.color.toUpperCase()}'s TURN `;
+        this.render();
+        // this is bound to window inside of event handlers thus using arrows
+        this.markersElement.addEventListener("click", (evt) => {
+            this.handleMarkerClick(evt);
+        });
+        this.boardElement.addEventListener("click", (evt) => {
+            this.handleMarkerClick(evt);
+        });
     }
-    return check(dest, direction, desired, ++matches);
-}
-//-------------------------------------------------------------//
-// If the number of matches found along combination of vectors eg
-// east and west is 3 or more the position is a winning position and
-// currentPlayer is the winner.
-//-------------------------------------------------------------//
-function isWinningPosition(pos) {
-    const horizontal = check(pos, west) + check(pos, east) + 1;
-    const vertical = check(pos, north) + check(pos, south) + 1;
-    const diagUp = check(pos, northEast) + check(pos, southWest) + 1;
-    const diagDown = check(pos, southEast) + check(pos, northWest) + 1;
-    return [horizontal, vertical, diagDown, diagUp].some(
-        (x) => x >= matchesToWin
-    );
+
+    render() {
+        this.renderBoard();
+        this.renderMessages();
+    }
+
+    renderBoard() {
+        this.board.forEach((colArr, colIdx) => {
+            colArr.forEach((el, idx) => {
+                document.getElementById(
+                    `c${colIdx}r${idx}`
+                ).style.backgroundColor = this.valueToColor(el);
+            });
+        });
+    }
+
+    renderMessages() {
+        this.messageElement.innerText = this.message;
+        this.messageElement.style.color = this.currentPlayer.color;
+    }
+
+    buildMarkers(width, markerEl) {
+        for (let i = 0; i < width; i++) {
+            let el = document.createElement("div");
+            el.id = i;
+            el.column = i;
+            markerEl.appendChild(el);
+        }
+    }
+
+    buildBoard(width, height, boardEl) {
+        for (let rowNumber = width - 1; rowNumber >= 0; rowNumber--) {
+            for (let columnNumber = 0; columnNumber < height; columnNumber++) {
+                let el = document.createElement("div");
+                el.id = `c${columnNumber}r${rowNumber}`;
+                el.row = rowNumber;
+                el.column = columnNumber;
+                boardEl.appendChild(el);
+            }
+        }
+    }
+
+    //TODO this shouldn't hardcode the 9 here and markers should be a child of game
+    setBoardColumnsStyle(width, boardEl, markerEl) {
+        boardEl.style.gridTemplateColumns = `repeat(${width},9vmin)`;
+        markerEl.style.gridTemplateColumns = `repeat(${width},9vmin)`;
+    }
+
+    clearBoardandMarkers() {
+        this.boardElement.innerHTML = "";
+        this.markersElement.innerHTML = "";
+    }
+
+    //------------------------------------------------------------//
+    // The core logic is contained herein.  Attempt to insert.
+    // If insertion succeeds test board along possible axis for 3 addition matching
+    // positions and either end the game or switch the active player.
+    //------------------------------------------------------------//
+
+    //ERROR this no longer points to object
+    handleMarkerClick(evt) {
+        //-------------------------------------------------------------//
+        // Only if clicking on a div and insertion succeeds
+        // meaning column wasn't full insert a value
+        //-------------------------------------------------------------//
+        if (evt.target.tagName !== "DIV") return; //click better
+        let latestInsertion = this.insert(
+            evt.target.column,
+            this.currentPlayer.boardValue
+        );
+        if (latestInsertion) {
+            if (this.isWinningPosition(latestInsertion)) this.endGame();
+            else this.switchPlayer();
+            this.render();
+        }
+    }
+
+    //-------------------------------------------------------------//
+    // Inserts number representing player into first unoccupied position
+    // in col, represented by current value being zero, and then returns
+    // the pos of most recent insertion both to see if insertion AND
+    // to test the value to see if player won
+    //-------------------------------------------------------------//
+    insert(col, n) {
+        let top = this.board[col].indexOf(0); //first zero is the first empty slot
+
+        //if not already full
+        if (top !== -1) {
+            this.board[col][top] = n;
+            {
+                let latestInsertion = { x: parseInt(col), y: top };
+                return latestInsertion;
+            }
+        } else return false;
+    }
+
+    //-------------------------------------------------------------//
+    // Takes position {x: n y: n} and direction {x: n y: n} and uses to them
+    // to recursively find the number of positions matching the value
+    // found at pos. It returns the current number of matches when we
+    // either run off the board, find a non-matching value, or get to 3
+    // indicating we have a total of 4 matching values in a line.
+    //-------------------------------------------------------------//
+    check(pos, direction, desired = this.board[pos.x][pos.y], matches = 0) {
+        let dest = addCords(direction, pos);
+        if (!this.positionInBounds(dest)) return matches;
+        let value = this.board[dest.x][dest.y];
+        if (desired !== value) {
+            return matches;
+        }
+        return this.check(dest, direction, desired, ++matches);
+    }
+    //-------------------------------------------------------------//
+    // If the number of matches found along combination of vectors eg
+    // east and west is 3 or more the position is a winning position and
+    // currentPlayer is the winner.
+    //-------------------------------------------------------------//
+    isWinningPosition(pos) {
+        const horizontal = this.check(pos, west) + this.check(pos, east) + 1;
+        const vertical = this.check(pos, north) + this.check(pos, south) + 1;
+        const diagUp =
+            this.check(pos, northEast) + this.check(pos, southWest) + 1;
+        const diagDown =
+            this.check(pos, southEast) + this.check(pos, northWest) + 1;
+        return [horizontal, vertical, diagDown, diagUp].some(
+            (x) => x >= this.matchesToWin
+        );
+    }
+
+    positionInBounds(pos) {
+        return (
+            pos.y >= 0 &&
+            pos.y < this.board[0].length &&
+            pos.x >= 0 &&
+            pos.x < this.board.length
+        );
+    }
+
+    switchPlayer() {
+        if (this.currentPlayer === this.players[0])
+            this.currentPlayer = this.players[1];
+        else this.currentPlayer = this.players[0];
+        this.message = `${this.currentPlayer.color.toUpperCase()}'s TURN `;
+    }
+
+    endGame() {
+        this.message = `${this.currentPlayer.color.toUpperCase()} WINS`;
+        //this removes anon event listeners
+        this.markersElement.outerHTML = this.markersElement.outerHTML;
+        this.boardElement.outerHTML = this.boardElement.outerHTML;
+    }
+    valueToColor(n) {
+        if (n === 0) return "white";
+        else return this.players.filter((x) => x.boardValue == n)[0].color;
+    }
 }
 
-function switchPlayer() {
-    if (currentPlayer === players[0]) currentPlayer = players[1];
-    else currentPlayer = players[0];
-    message = `${currentPlayer.color.toUpperCase()}'s TURN `;
-}
-
-function endGame() {
-    message = `${currentPlayer.color.toUpperCase()} WINS`;
-    markersElement.removeEventListener("click", handleMarkerClick);
-    boardEl.removeEventListener("click", handleMarkerClick);
-}
-
-function valueToColor(n) {
-    if (n === 0) return "white";
-    else return players.filter((x) => x.boardValue == n)[0].color;
-}
-
-function positionInBounds(pos) {
-    return (
-        pos.y >= 0 &&
-        pos.y < board[0].length &&
-        pos.x >= 0 &&
-        pos.x < board.length
-    );
+class Area {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
 }
 
 function addCords(obA, obB) {
@@ -149,80 +284,14 @@ function repeat(x, times) {
     return res;
 }
 
-function buildBoard(width, height, boardEl) {
-    for (let rowNumber = width - 1; rowNumber >= 0; rowNumber--) {
-        for (let columnNumber = 0; columnNumber < height; columnNumber++) {
-            let el = document.createElement("div");
-            el.id = `c${columnNumber}r${rowNumber}`;
-            el.row = rowNumber;
-            el.column = columnNumber;
-            boardEl.appendChild(el);
-        }
-    }
-}
-function buildMarkers(width, markerEl) {
-    for (let i = 0; i < width; i++) {
-        let el = document.createElement("div");
-        el.id = i;
-        el.column = i;
-        markerEl.appendChild(el);
-    }
-}
-
-function clearBoardandMarkers() {
-    boardEl.innerHTML = "";
-    markersElement.innerHTML = "";
-}
-
-//TODO this shouldn't hardcode the 9 here and markers should be a child of game
-function setBoardColumnsStyle(width, boardEl, markerEl) {
-    boardEl.style.gridTemplateColumns = `repeat(${width},9vmin)`;
-    markerEl.style.gridTemplateColumns = `repeat(${width},9vmin)`;
-}
-
 function init() {
-    clearBoardandMarkers();
-    players = [
-        { name: "player-one", color: "purple", boardValue: 1 },
-        { name: "player-two", color: "gold", boardValue: 2 },
-    ];
-
-    numberOfColumns = 9;
-    numberOfRows = 6;
-    matchesToWin = 5;
-    board = [];
-    let column = repeat(0, numberOfRows);
-    for (let i = 0; i < numberOfColumns; i++) {
-        board.push(column.slice(0));
-    }
-    buildBoard(numberOfRows, numberOfColumns, boardEl);
-    buildMarkers(numberOfColumns, markersElement);
-    setBoardColumnsStyle(numberOfColumns, boardEl, markersElement);
-
-    currentPlayer = players[0];
-    winner = null;
-    message = `${currentPlayer.color.toUpperCase()}'s TURN `;
-    render();
-    markersElement.addEventListener("click", handleMarkerClick);
-    boardEl.addEventListener("click", handleMarkerClick);
-}
-function render() {
-    renderBoard();
-    renderMessages();
-}
-
-function renderBoard() {
-    board.forEach((colArr, colIdx) => {
-        colArr.forEach((el, idx) => {
-            document.getElementById(`c${colIdx}r${idx}`).style.backgroundColor =
-                valueToColor(el);
-        });
-    });
-}
-
-function renderMessages() {
-    messageElement.innerText = message;
-    messageElement.style.color = currentPlayer.color;
+    if (typeof game !== "undefined") game.clearBoardandMarkers();
+    game = new Game(boardEl, markersElement, messageElement, "title", 4, 4, 4, [
+        "purple",
+        "gold",
+    ]);
 }
 
 init();
+
+document.getElementById("reset_button").addEventListener("click", init);
